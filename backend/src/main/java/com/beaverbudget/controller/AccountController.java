5fa23e3903c117c3e458bc35101ecbfe79a3cf77
castor-dev <93452119+castor-dev@users.noreply.github.com>
2025-10-14 15:00:58 +0200

    4. Account web layer

--- backend/src/main/java/com/beaverbudget/controller/AccountController.java
+++ backend/src/main/java/com/beaverbudget/controller/AccountController.java
@@ -2,71 +2,90 @@ package com.beaverbudget.controller;
 
 import com.beaverbudget.api.AccountsApi;
 import com.beaverbudget.mapper.MapperProvider;
+import com.beaverbudget.model.Account;
 import com.beaverbudget.model.AccountDTO;
+import com.beaverbudget.model.Transaction;
 import com.beaverbudget.model.TransactionDTO;
 import com.beaverbudget.service.AccountService;
-import com.beaverbudget.service.TransactionService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+/**
+ * Controller for account resource requests
+ */
 @RestController
 public class AccountController implements AccountsApi {
 
     private final AccountService accountService;
-    private final TransactionService transactionService;
     private final MapperProvider mapperProvider;
 
-    public AccountController(AccountService accountService, TransactionService transactionService, MapperProvider mapperProvider) {
+    /**
+     * Class constructor
+     * @param accountService {@link AccountService}
+     * @param mapperProvider {@link MapperProvider}
+     */
+    public AccountController(AccountService accountService, MapperProvider mapperProvider) {
         this.accountService = accountService;
-        this.transactionService = transactionService;
         this.mapperProvider = mapperProvider;
     }
 
 
     @Override
     public ResponseEntity<AccountDTO> createAccount(AccountDTO accountDTO) {
-        return AccountsApi.super.createAccount(accountDTO);
+        Account savedAccount = accountService.createAccount(mapperProvider.map(accountDTO, Account.class));
+        return ResponseEntity.ok(mapperProvider.map(savedAccount, AccountDTO.class));
     }
 
     @Override
     public ResponseEntity<AccountDTO> getAccountById(Integer accountId) {
-        return AccountsApi.super.getAccountById(accountId);
+        Account account = accountService.findAccountById(accountId);
+        return ResponseEntity.ok(mapperProvider.map(account, AccountDTO.class));
     }
 
     @Override
     public ResponseEntity<List<AccountDTO>> listAccounts() {
-        return AccountsApi.super.listAccounts();
+        List<AccountDTO> accountDTOList = accountService.findAllAccounts().stream()
+                .map(account -> mapperProvider.map(account, AccountDTO.class))
+                .toList();
+        return ResponseEntity.ok(accountDTOList);
     }
 
     @Override
     public ResponseEntity<AccountDTO> updateAccount(Integer accountId, AccountDTO accountDTO) {
-        return AccountsApi.super.updateAccount(accountId, accountDTO);
+        Account updateAccount = accountService.updateAccount(accountId, mapperProvider.map(accountDTO, Account.class));
+        return ResponseEntity.ok(mapperProvider.map(updateAccount, AccountDTO.class));
     }
 
     @Override
     public ResponseEntity<Void> deleteAccount(Integer accountId) {
-        return AccountsApi.super.deleteAccount(accountId);
+        accountService.deleteAccount(accountId);
+        return ResponseEntity.noContent().build();
     }
 
     @Override
     public ResponseEntity<TransactionDTO> createAccountTransaction(Integer accountId, TransactionDTO transactionDTO) {
-        return AccountsApi.super.createAccountTransaction(accountId, transactionDTO);
+        Transaction transaction = mapperProvider.map(transactionDTO, Transaction.class);
+        Transaction accountTransaction = accountService.createAccountTransaction(accountId, transaction);
+        return ResponseEntity.ok(mapperProvider.map(accountTransaction, TransactionDTO.class));
     }
 
     @Override
     public ResponseEntity<TransactionDTO> getAccountTransactionById(Integer accountId, Integer transactionId) {
-        return AccountsApi.super.getAccountTransactionById(accountId, transactionId);
+        Transaction accountTransaction = accountService.findAccountTransactionById(accountId, transactionId);
+        return ResponseEntity.ok(mapperProvider.map(accountTransaction, TransactionDTO.class));
     }
 
     @Override
     public ResponseEntity<TransactionDTO> updateAccountTransaction(Integer accountId, Integer transactionId, TransactionDTO transactionDTO) {
-        return AccountsApi.super.updateAccountTransaction(accountId, transactionId, transactionDTO);
+        Transaction transaction = accountService.updateAccountTransaction(accountId, transactionId, mapperProvider.map(transactionDTO, Transaction.class));
+        return ResponseEntity.ok(mapperProvider.map(transaction, TransactionDTO.class));
     }
 
     @Override
     public ResponseEntity<Void> deleteAccountTransaction(Integer accountId, Integer transactionId) {
-        return AccountsApi.super.deleteAccountTransaction(accountId, transactionId);
+        accountService.deleteAccountTransaction(accountId, transactionId);
+        return ResponseEntity.noContent().build();
     }
 }
