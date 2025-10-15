25c4b6d3225d066fe98acacc00af1f4217a9a31a
castor-dev <93452119+castor-dev@users.noreply.github.com>
2025-10-15 19:27:03 +0200

    8. transaction service

--- backend/src/main/java/com/beaverbudget/persistence/TransactionPersistenceService.java
+++ backend/src/main/java/com/beaverbudget/persistence/TransactionPersistenceService.java
@@ -29,5 +29,20 @@ public interface TransactionPersistenceService {
         throw new FeatureNotImplementedException("TransactionPersistenceService.deleteTransaction not implemented");
     }
 
+    default Optional<Transaction> findTransactionById(Integer transactionId){
+        throw new FeatureNotImplementedException("TransactionPersistenceService.findTransactionById not implemented");
+    }
+
+    default List<Transaction> findAllTransactions(LocalDate from, LocalDate to){
+        throw new FeatureNotImplementedException("TransactionPersistenceService.findAllTransactions not implemented");
+    }
+
+    default Transaction updateTransaction(Integer transactionId, Transaction transaction){
+        throw new FeatureNotImplementedException("TransactionPersistenceService.updateTransaction not implemented");
+    }
+
+    default void deleteTransaction(Integer transactionId){
+        throw new FeatureNotImplementedException("TransactionPersistenceService.deleteTransaction not implemented");
+    }
 
 }
--- backend/src/main/java/com/beaverbudget/service/impl/TransactionServiceImpl.java
+++ backend/src/main/java/com/beaverbudget/service/impl/TransactionServiceImpl.java
@@ -1,7 +1,77 @@
 package com.beaverbudget.service.impl;
 
+import com.beaverbudget.exceptions.InvalidResourceException;
+import com.beaverbudget.exceptions.ResourceNotFoundException;
+import com.beaverbudget.model.Transaction;
+import com.beaverbudget.persistence.AccountPersistenceService;
+import com.beaverbudget.persistence.TransactionPersistenceService;
 import com.beaverbudget.service.TransactionService;
+import lombok.AllArgsConstructor;
+import org.apache.logging.log4j.util.Strings;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.util.List;
+
+import static java.util.Objects.isNull;
+import static java.util.Objects.nonNull;
+
 @Service
-public class TransactionServiceImpl implements TransactionService {}
+@AllArgsConstructor
+public class TransactionServiceImpl implements TransactionService {
+
+    private final TransactionPersistenceService transactionPersistenceService;
+    private final AccountPersistenceService accountPersistenceService;
+
+    @Override
+    public Transaction findTransactionById(Integer transactionId) {
+        return transactionPersistenceService.findTransactionById(transactionId)
+                .orElseThrow(() -> new ResourceNotFoundException(String.format("Transaction with id %s not found", transactionId)));
+    }
+
+    @Override
+    public List<Transaction> findTransactions(LocalDate from, LocalDate to) {
+        return transactionPersistenceService.findAllTransactions(from, to);
+    }
+
+    @Override
+    public void deleteTransaction(Integer transactionId) {
+        transactionPersistenceService.deleteTransaction(transactionId);
+    }
+
+    @Override
+    public Transaction updateTransaction(Integer transactionId, Transaction transaction) {
+
+        Transaction existingTransaction = transactionPersistenceService.findTransactionById(transactionId)
+                .orElseThrow(() -> new ResourceNotFoundException(String.format("Transaction %s not found ", transactionId)));
+
+        if (nonNull(transaction.getAmount())) {
+            existingTransaction.setAmount(transaction.getAmount());
+        }
+        if (Strings.isNotBlank(transaction.getDescription())) {
+            existingTransaction.setDescription(transaction.getDescription());
+        }
+        if (nonNull(transaction.getPlanned())) {
+            existingTransaction.setPlanned(transaction.getPlanned());
+        }
+        if (nonNull(transaction.getParentTransaction())) {
+            Integer parentTransactionId = transaction.getParentTransaction().getId();
+            if (isNull(parentTransactionId)) {
+                throw new InvalidResourceException("Invalid parent transaction");
+            }
+            Transaction parentTransaction = transactionPersistenceService.findTransactionById(parentTransactionId)
+                    .orElseThrow(() -> new ResourceNotFoundException(String.format("Transaction %s not found", parentTransactionId)));
+            existingTransaction.setParentTransaction(parentTransaction);
+        }
+        if (nonNull(transaction.getAccountId())) {
+            Integer accountId = transaction.getAccountId();
+            accountPersistenceService.findAccountById(accountId)
+                    .orElseThrow(() -> new ResourceNotFoundException(String.format("Account %s not found", accountId)));
+            existingTransaction.setAccountId(accountId);
+        }
+
+        return transactionPersistenceService.saveTransaction(existingTransaction);
+    }
+
+
+}
