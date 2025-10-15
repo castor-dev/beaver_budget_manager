79793d218330cfde3ee1e595f8e40c85c87abb8c
castor-dev <93452119+castor-dev@users.noreply.github.com>
2025-10-15 16:53:22 +0200

    7. unit tests

---
+++ backend/src/main/java/com/beaverbudget/persistence/impl/TransactionPersistenceServiceImpl.java
@@ -0,0 +1,10 @@
+package com.beaverbudget.persistence.impl;
+
+import com.beaverbudget.persistence.TransactionPersistenceService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class TransactionPersistenceServiceImpl implements TransactionPersistenceService {
+}
