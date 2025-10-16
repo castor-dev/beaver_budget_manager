918cf80e7867999c51fbd023985cb2d63abb2b5d
castor-dev <93452119+castor-dev@users.noreply.github.com>
2025-10-16 08:00:49 +0200

    19. flyway config

--- backend/src/main/java/com/beaverbudget/BeaverBudgetApplication.java
+++ backend/src/main/java/com/beaverbudget/BeaverBudgetApplication.java
@@ -2,10 +2,14 @@ package com.beaverbudget;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
 
 @SpringBootApplication
 public class BeaverBudgetApplication {
-  public static void main(String[] args) {
-    SpringApplication.run(BeaverBudgetApplication.class, args);
-  }
+    public static void main(String[] args) {
+        ConfigurableApplicationContext ctx = SpringApplication.run(BeaverBudgetApplication.class, args);
+        System.out.println("🟢 Flyway bean: " + ctx.containsBean("flyway"));
+    }
 }
+
---
+++ backend/src/main/resources/db/migration/V4__create_transaction_table.sql
@@ -0,0 +1,18 @@
+CREATE TABLE transactions (
+    id                  SERIAL PRIMARY KEY,
+    account_id          INTEGER NOT NULL,
+    amount              NUMERIC(15, 2) NOT NULL,
+    type                VARCHAR(20) NOT NULL CHECK (type IN ('INCOME', 'EXPENSE')),
+    date                TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    description         VARCHAR(255),
+    planned             BOOLEAN DEFAULT FALSE,
+    parent_transaction  INTEGER NULL,
+
+    CONSTRAINT fk_transaction_account
+        FOREIGN KEY (account_id) REFERENCES accounts(id)
+        ON DELETE CASCADE,
+
+    CONSTRAINT fk_transaction_parent
+        FOREIGN KEY (parent_transaction) REFERENCES transactions(id)
+        ON DELETE SET NULL
+);
