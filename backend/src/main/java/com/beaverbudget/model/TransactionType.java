ab643fc4aabc357795252e418546145f10ad281b
castor-dev <93452119+castor-dev@users.noreply.github.com>
2025-10-16 21:40:39 +0200

    21. Application entities

---
+++ backend/src/main/java/com/beaverbudget/model/TransactionType.java
@@ -0,0 +1,7 @@
+package com.beaverbudget.model;
+
+public enum TransactionType {
+    INCOME,
+    EXPENSE,
+    TRANSFER
+}
--- backend/src/main/resources/db/migration/V4__create_transaction_table.sql
+++ backend/src/main/resources/db/migration/V4__create_transaction_table.sql
@@ -6,13 +6,13 @@ CREATE TABLE transactions (
     date                TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     description         VARCHAR(255),
     planned             BOOLEAN DEFAULT FALSE,
-    parent_transaction  INTEGER NULL,
+    parent_transaction_id  INTEGER NULL,
 
     CONSTRAINT fk_transaction_account
         FOREIGN KEY (account_id) REFERENCES accounts(id)
         ON DELETE CASCADE,
 
     CONSTRAINT fk_transaction_parent
-        FOREIGN KEY (parent_transaction) REFERENCES transactions(id)
+        FOREIGN KEY (parent_transaction_id) REFERENCES transactions(id)
         ON DELETE SET NULL
 );
