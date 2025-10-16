8d9caff6d3eecc16273faf98fb5f90fc12e73dd1
castor-dev <93452119+castor-dev@users.noreply.github.com>
2025-10-16 16:33:07 +0200

    h2 dependency for tests

--- backend/src/test/java/com/beaverbudget/BeaverBudgetApplicationTests.java
+++ backend/src/test/java/com/beaverbudget/BeaverBudgetApplicationTests.java
@@ -2,8 +2,10 @@ package com.beaverbudget;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class BeaverBudgetApplicationTests {
 
   @Test
